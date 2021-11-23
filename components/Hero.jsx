22a56c8f810c1954c71b8d976ebd031495f74d4a
Angus Lam <angus@hey.com>
2021-11-22 22:19:50 -0500

    Updated about and portfolio on landing page

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -11,7 +11,7 @@ export default function Hero() {
       </div>
       <div className="flex flex-1">
         <div className="flex items-center p-4 mb-8 w-full">
-          <div className="m-auto max-w-screen-md">
+          <div className="m-auto max-w-screen-sm">
             <div className="md:flex">
               <div className="border border-white flex-1">
                 <h2 className="font-black p-2 text-white title-bg">About</h2>
@@ -21,54 +21,50 @@ export default function Hero() {
                     Full stack software engineer with an affinity for building
                     products. Currently at Stripe.
                   </p>
-                  <p>
-                    <a className="text-blue-400 underline" href="#">
-                      Contact me
-                    </a>
-                  </p>
+                  <div className="text-white">
+                    <ul className="list-disc ml-6">
+                      <li>
+                        <a className="text-blue-400 underline" href="#">
+                          Twitter
+                        </a>
+                      </li>
+                      <li>
+                        <a className="text-blue-400 underline" href="#">
+                          GitHub
+                        </a>
+                      </li>
+                      <li>
+                        <a className="text-blue-400 underline" href="#">
+                          LinkedIn
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
               <div className="border-b border-r md:border-t border-l md:border-l-0 border-white flex-2">
                 <h2 className="font-black p-2 text-white title-bg">
-                  Past work
+                  Portfolio
                 </h2>
                 <div className="border-b border-white"></div>
-                <p className="p-2 text-white">
-                  Full stack software engineer with an affinity for building
-                  products. Currently at Stripe.
-                </p>
-              </div>
-            </div>
-            <div className="border-b border-l border-r border-white flex">
-              <div className="flex-1">
-                <h2 className="font-black p-2 text-white title-bg">Skills</h2>
-                <div className="border-b border-white"></div>
-                <div className="p-2">
-                  <p className="text-white">
-                    <span className="font-bold text-sm text-gray-400 uppercase">
-                      Languages
-                    </span>{" "}
-                    Java, JavaScript, Python, Ruby
-                  </p>
-                  <p className="text-white">
-                    <span className="font-bold text-sm text-gray-400 uppercase">
-                      Full stack
-                    </span>{" "}
-                    CSS, Express, Flask, jQuery, MongoDB, Node.js, React, React
-                    Native, Webpack
-                  </p>
-                  <p className="text-white">
-                    <span className="font-bold text-sm text-gray-400 uppercase">
-                      Game dev
-                    </span>{" "}
-                    Unity3D, libGDX
-                  </p>
-                  <p className="text-white">
-                    <span className="font-bold text-sm text-gray-400 uppercase">
-                      Design
-                    </span>{" "}
-                    Adobe Illustrator, Adobe Photoshop
-                  </p>
+                <div className="p-2 space-y-2">
+                  <a className="flex space-x-2 text-white" href="#">
+                    <div className="bg-white h-20 flex-1 max-w-1/2"></div>
+                    <p className="flex-1">Netscrape</p>
+                  </a>
+                  <a className="flex space-x-2 text-white" href="#">
+                    <div className="bg-white h-20 flex-1 max-w-1/2"></div>
+                    <p className="flex-1">Ghosts</p>
+                  </a>
+                  <a className="flex space-x-2 text-white" href="#">
+                    <div className="bg-white h-20 flex-1 max-w-1/2"></div>
+                    <p className="flex-1">Self-regulating hydroponics</p>
+                  </a>
+                </div>
+                <div className="pb-2 px-2">
+                  <a className="text-blue-400 underline" href="#">
+                    More…
+                  </a>
                 </div>
               </div>
             </div>
@@ -95,7 +91,7 @@ export default function Hero() {
                     </li>
                     <li>
                       <a className="text-blue-400 underline" href="#">
-                        more…
+                        More…
                       </a>
                     </li>
                   </ul>
