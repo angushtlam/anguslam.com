4c61ac855e2436f3aa18dbb844c586ca9aa3ea3a
angushtlam <>
2022-11-27 17:21:34 +0000

    Filter high cost items

--- notion.ts
+++ notion.ts
@@ -10,26 +10,28 @@ async function personalItemLogDatabase() {
   });
 
   let tableLastUpdated = new Date();
-  const items = results.map((item: any) => {
-    const costPerDay = (item.properties.Cost.number / item.properties['Days owned'].formula.number) + Number.EPSILON;
-    const daysUntilTarget = Math.round(item.properties.Cost.number - item.properties['Days owned'].formula.number);
-
-    const lastEditedTime = new Date(item.last_edited_time);
-    if (tableLastUpdated > lastEditedTime) {
-      tableLastUpdated = lastEditedTime;
-    }
-
-    return ({
-      id: item.id,
-      categories: item.properties.Category.multi_select.filter((x: any) => ({
-        color: x.color,
-        name: x.name,
-      })),
-      costPerDay: Math.round(costPerDay * 100) / 100,
-      daysUntilTarget,
-      name: item.properties.Name.title[0].plain_text,
-    })
-  })
+  const items = results
+      .filter((item: any) => item.properties.Cost.number && item.properties.Cost.number < 500)
+      .map((item: any) => {
+        const costPerDay = (item.properties.Cost.number / item.properties['Days owned'].formula.number) + Number.EPSILON;
+        const daysUntilTarget = Math.round(item.properties.Cost.number - item.properties['Days owned'].formula.number);
+    
+        const lastEditedTime = new Date(item.last_edited_time);
+        if (tableLastUpdated > lastEditedTime) {
+          tableLastUpdated = lastEditedTime;
+        }
+    
+        return ({
+          id: item.id,
+          categories: item.properties.Category.multi_select.filter((x: any) => ({
+            color: x.color,
+            name: x.name,
+          })),
+          costPerDay: Math.round(costPerDay * 100) / 100,
+          daysUntilTarget,
+          name: item.properties.Name.title[0].plain_text,
+        })
+      })
 
   return {
     items,
